24b1e83d48b42e26ff9e5e8aa8ef06c3c765e317
Naeem <naeemark@gmail.com>
2019-01-26 21:13:18 +0800

    succefful code coverage without uiTests

--- app/src/test/kotlin/com/kotlindemo/model/UserTest.kt
+++ app/src/test/kotlin/com/kotlindemo/model/UserTest.kt
@@ -1,17 +1,22 @@
 package com.kotlindemo.model
 
 import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotEquals
 import org.junit.Before
 import org.junit.Test
 
-import org.junit.Assert.*
-
 class UserTest {
 
+    val EXPECTED_FIRST_EMAIL: String = "Satesh"
+    val EXPECTED_LAST_PASSWORD: String = "Anumala"
 
+    protected lateinit var user: User
 
     @Before
     fun setUp() {
+
+        user = User(EXPECTED_FIRST_EMAIL, EXPECTED_LAST_PASSWORD)
     }
 
     @After
@@ -20,14 +25,19 @@ class UserTest {
 
     @Test
     fun isDataValid() {
+
     }
 
     @Test
     fun getValidationCode() {
+
     }
 
     @Test
     fun getEmail() {
+
+        assertNotEquals(user.getEmail(), "OK")
+        assertEquals(user.getEmail(), EXPECTED_FIRST_EMAIL)
     }
 
     @Test
