f1b67773d6690b08c2483cb73905c6a43698ac7d
Naeem <naeemark@gmail.com>
2019-01-27 18:20:26 +0800

    renames sonar->jacoco

---
+++ app/src/test/kotlin/com/kotlindemo/model/viewModel/LoginViewModelTest.kt
@@ -0,0 +1,46 @@
+package com.kotlindemo.model.viewModel
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class LoginViewModelTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun getSuccess() {
+    }
+
+    @Test
+    fun getFailure() {
+    }
+
+    @Test
+    fun getSuccessObservable() {
+    }
+
+    @Test
+    fun getFailureObservable() {
+    }
+
+    @Test
+    fun getEmailTextWatcher() {
+    }
+
+    @Test
+    fun getPasswordTextWatcher() {
+    }
+
+    @Test
+    fun onLoginClicked() {
+    }
+}
