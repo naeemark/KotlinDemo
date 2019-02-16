059c825e6df9047860ae8b9148a64bff51c19c1f
Naeem <naeemark@gmail.com>
2019-02-16 17:09:35 +0800

    converts to RxKotlin

--- app/src/main/java/com/kotlindemo/MainActivity.kt
+++ app/src/main/java/com/kotlindemo/MainActivity.kt
@@ -1,12 +1,10 @@
 package com.kotlindemo
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import io.reactivex.Observable
-import io.reactivex.Observer
-import io.reactivex.disposables.Disposable
+import io.reactivex.rxkotlin.subscribeBy
+import io.reactivex.rxkotlin.toObservable
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,47 +12,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
     fun startRStream(view: View) {
 
-        // create an Observable
-        val myObservable = getObservable()
-        
-        // create an Observer
-        val myObserver = getObserver()
+        val list = listOf("1", "2", "3", "4", "5")
 
-
-        // subscribe myObservable to myObserver
-        myObservable.subscribe(myObserver)
-    }
-
-    private fun getObservable(): Observable<String> {
-
-        Log.d(TAG, "getObservable: ")
-        return Observable.just("1", "2", "3", "4", "5")
-    }
-
-
-    private fun getObserver(): Observer<String> {
-
-        return object : Observer<String>{
-
-            override fun onSubscribe(d: Disposable) {
-
-                Log.d(TAG, "onSubscribe: ")
-            }
-
-            override fun onNext(t: String) {
-                Log.d(TAG, "onNext: $t")
-            }
-
-            override fun onError(e: Throwable) {
-                Log.d(TAG, "onError: $e")
-            }
-
-            override fun onComplete() {
-                Log.d(TAG, "onComplete: ")
-            }
-        }
+        val observable = list.toObservable()
+        observable.subscribeBy(
+            onNext = { println(it)},
+            onError = {it.printStackTrace()},
+            onComplete = { println("onComplete")}
+        ).dispose()
     }
 
     companion object {
